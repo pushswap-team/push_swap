7d7c5a9452ed86edf08e7f23a90207c79ef43578
Baran Dayaklı <barandayakli9@gmail.com>
2026-04-15 12:43:56 +0300

    modified:   ft_sort_complex.c.

--- ft_sort_complex.c
+++ ft_sort_complex.c
@@ -14,12 +14,51 @@
 
 int     ft_get_max_bits(t_stack *a)
 {
+	int	max;
+	int	bits;
+	int	i;
 
+	if (!a || a->size < 2 || !(a->array))
+		return (0);
+	max = a->array[0];
+	i = 1;
+	while (i < a->size)
+	{
+		if (a->array[i] > max)
+			max = a->array[i];
+		i++;
+	}
+	bits = 0;
+	while (max > 0)
+	{
+		bits++;
+		max >>= 1;
+	}
+	return (bits);
 }
 
 void    ft_radix_pass(t_data *d, int bit)
 {
+	int	temp;
+	int	i;	
 
+	if (!d || !(d->a) || !(d->b)
+		|| !((d->a)->array) ||!((d->b)->array))
+		return ;
+	temp = (d->a)->size;
+	i = 0;
+	while (i < temp)
+	{
+		if ((((d->a)->array[0] >> bit) & 1) == 0)
+			ft_pb(d);
+		else
+			ft_ra(d);
+		i++;
+	}
+	while ((d->b)->size != 0)
+	{
+		ft_pa(d);
+	}
 }
 
 void    ft_sort_complex(t_data *d)
